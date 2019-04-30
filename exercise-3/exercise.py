50c3585239bbc63507336e0c48362bbde3a956b1
Tianxin Song <tianxins@uw.edu>
2019-04-30 10:36:57 -0700

    Add the in-class note jupyter notebook

--- exercise-3/exercise.py
+++ exercise-3/exercise.py
@@ -17,6 +17,30 @@
 # Be sure to consider both positive AND negative distances!
 # BONUS: Also print out whether the guess is high or low
 
+def print_hot_or_cold(target, guess):
+    difference = abs(target - guess)
+    if difference == 0: 
+        print("got it!")
+    elif difference <= 1 and difference > 0:
+        print("scalding hot")
+    elif difference <= 3:
+        print("very warm")
+    elif difference <= 5:
+        print("warm")
+    elif difference <= 8:
+        print("cold")
+    elif difference <= 13:
+        print("very cold")
+    else: #difference > 13:
+        print("icy freezing miserably cold")
+   
+
+
+
+#print_hot_or_cold(10, 8)
+#print_hot_or_cold(10, 9)
+#print_hot_or_cold(10, 10)
+
 
 
 # Define a function `guess_number()` that takes a single argument (a target number)
@@ -29,8 +53,25 @@
 # or by simply calling your `guess_number() method again IF the user didn't get
 # the answer right. The later is an example of **recursion**.
 
+def guess_number(target_number):
+    response = int(input("Guess a number:"))
+    print_hot_or_cold(target_number, response)
 
+#if the answer is not correct, 
+    if response != target_number:
+        #ask again
+        guess_number(target_number)
+
+
+#guess_number(10)
 
 # If the file is run as a top-level script, your script should pick a random number
 # between 1 and 50 as the target and then start the game. You should inform the
 # user of the range of numbers before asking them for a guess.
+
+if __name__ == "__main__":
+    import random
+    random_number = random.randint(1,50)
+    print("Guess the number between 1 and 50")
+    guess_number(random_number)
+
